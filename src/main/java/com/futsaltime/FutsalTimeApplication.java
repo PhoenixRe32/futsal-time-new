969f058240f2295d650e5c2d539ab2e9741fff67
Andreas Andreou <aandreou@thoughtworks.com>
2017-06-24 16:02:59 +0100

    Enabled actuator so I can see mappings and config loaded.

--- src/main/java/com/futsaltime/FutsalTimeApplication.java
+++ src/main/java/com/futsaltime/FutsalTimeApplication.java
@@ -3,10 +3,21 @@ package com.futsaltime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+
 @SpringBootApplication
 public class FutsalTimeApplication {
 
 	public static void main(String[] args) {
+		ClassLoader cl = ClassLoader.getSystemClassLoader();
+
+		URL[] urls = ((URLClassLoader)cl).getURLs();
+
+		for(URL url: urls){
+			System.out.println(url.getFile());
+		}
+
 		SpringApplication.run(FutsalTimeApplication.class, args);
 	}
 }
