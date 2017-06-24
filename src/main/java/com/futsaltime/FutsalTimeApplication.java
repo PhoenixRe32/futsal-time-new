c2e7182fec02374a0715bfee859efdecc8befa7c
Andreas Andreou <aandreou@thoughtworks.com>
2017-06-24 14:43:09 +0100

    Service lists facilities in system.

--- src/main/java/com/futsaltime/FutsalTimeApplication.java
+++ src/main/java/com/futsaltime/FutsalTimeApplication.java
@@ -1,4 +1,4 @@
-package com.futsaltime.futsaltime;
+package com.futsaltime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/futsaltime/facility/FacilityController.java
@@ -0,0 +1,19 @@
+package com.futsaltime.facility;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+@RequestMapping("facilities")
+public class FacilityController {
+
+	@RequestMapping(method = GET, path = "list")
+	public Collection<String> listFacilities() {
+		return Arrays.asList("Facility A", "Facility B", "Facility C");
+	}
+}
