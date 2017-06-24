02448773f52e7fe2192df008ea24798f144f14e8
Andreas Andreou <aandreou@thoughtworks.com>
2017-06-24 13:27:07 +0100

    Cleaning up folder

--- src/main/java/com/futsaltime/futsaltime/FutsalTimeApplication.java
+++ src/main/java/com/futsaltime/futsaltime/FutsalTimeApplication.java
@@ -1,12 +1,12 @@
-package com.example.demo;
+package com.futsaltime.futsaltime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class FutsalTimeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(FutsalTimeApplication.class, args);
 	}
 }
